ad1e1d0a3e83fadcb0c44d75f6fffaa940ce33e8
William Darian <williamdariansutedjo@gmail.com>
2019-10-18 23:56:40 +0700

    Ren'Py 7.3.5

--- renpy/tools/chocolateyinstall.ps1
+++ renpy/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName   = 'renpy'
   fileType      = 'exe'
-  url           = 'https://www.renpy.org/dl/7.3.2/renpy-7.3.2-sdk.7z.exe'
+  url           = 'https://www.renpy.org/dl/7.3.5/renpy-7.3.5-sdk.7z.exe'
   softwareName  = 'renpy*'
-  checksum      = '8d6e511ce39324e1ea2d659af727e3bbf7969f10fef1a313241391e2a6ff77a6'
+  checksum      = '3f2760be6c8b36698308470947783b1f5ce7ebcb4e2ae6bf2761212f5c925823'
   checksumType  = 'sha256'
   silentArgs    = "-y -gm2"
   validExitCodes= @(0)
