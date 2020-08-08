476d41fc1b86ee7e4d5a0b7ac443f433a48bdaff
William Darian <williamdariansutedjo@gmail.com>
2020-08-08 21:33:34 +0700

    LibreSprite 1.0

---
+++ libresprite/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = 'libresprite'
+  fileType      = 'exe'
+  url           = 'https://github.com/LibreSprite/LibreSprite/releases/download/_tmp/LibreSpriteWin.exe'
+  softwareName  = 'libresprite*'
+  checksum      = '0b97559ddb9979f67836941b52caadafba37c22b639aa8bc0643cd17916a5324'
+  checksumType  = 'sha256'
+  silentArgs    = "-y -gm2"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ libresprite/update.ps1
@@ -0,0 +1,26 @@
+import-module au
+
+$releases = 'https://libresprite.github.io'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+        }
+    }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $url32   = $download_page.links | ? href -match '\.exe$' | % href | Select-Object -First 1
+    # $version = Split-Path ( Split-Path $url32 ) -Leaf
+
+    @{
+        URL32   = $url32
+        Version = '1.0'
+    }
+}
+
+update
