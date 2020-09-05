31acddffa3276f900f18564119c9e273f0423df6
Manuel Thalmann <m@nuth.ch>
2020-09-05 19:24:58 +0200

    Fix incorrect installation location

--- renpy/tools/chocolateyinstall.ps1
+++ renpy/tools/chocolateyinstall.ps1
@@ -1,4 +1,5 @@
 ﻿$ErrorActionPreference = 'Stop';
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition;
 
 $packageArgs = @{
   packageName   = 'renpy'
@@ -7,7 +8,7 @@ $packageArgs = @{
   softwareName  = 'renpy*'
   checksum      = '3f2760be6c8b36698308470947783b1f5ce7ebcb4e2ae6bf2761212f5c925823'
   checksumType  = 'sha256'
-  silentArgs    = "-y -gm2"
+  silentArgs    = "-y -gm2 -o$toolsPath"
   validExitCodes= @(0)
 }
 
