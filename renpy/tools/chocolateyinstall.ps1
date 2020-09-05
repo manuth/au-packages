53d3a6ae806a81444e3aef98aa032de3a30565d4
Manuel Thalmann <m@nuth.ch>
2020-09-05 19:32:28 +0200

    Make the installer completely silent

--- renpy/tools/chocolateyinstall.ps1
+++ renpy/tools/chocolateyinstall.ps1
@@ -1,15 +1,12 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition;
 
-$packageArgs = @{
+$archiveArgs = @{
   packageName   = 'renpy'
-  fileType      = 'exe'
   url           = 'https://www.renpy.org/dl/7.3.5/renpy-7.3.5-sdk.7z.exe'
-  softwareName  = 'renpy*'
   checksum      = '3f2760be6c8b36698308470947783b1f5ce7ebcb4e2ae6bf2761212f5c925823'
   checksumType  = 'sha256'
-  silentArgs    = "-y -gm2 -o$toolsPath"
-  validExitCodes= @(0)
+  unzipLocation = $toolsPath
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @archiveArgs
